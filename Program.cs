9640fa2e89d8972defdd326434a4190c74f749ea
Gangadevi Ch <chilamakooru.gangaiiit@gmail.com>
2022-02-18 20:41:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace probles
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to day 6 assignments");
+        }
+    }
+}
