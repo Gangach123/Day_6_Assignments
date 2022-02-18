857663e5c42bd9bb41b75f46540567852a825d04
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-18 21:51:36 +0530

    Perfect_Number

--- Program.cs
+++ Program.cs
@@ -5,20 +5,29 @@ namespace probles
     {
         static void Main(string[] args)
         {
-            int first=0;
-            int second=1;
-            int third=0;
+            int a=0;
+
             Console.Write("Enter a number: ");
             int number = Convert.ToInt32(Console.ReadLine());
-            Console.Write(first + " " + second + " ");
-            for (int i = 2; i < number; i++)
+            
+            for (int i = 1; i < number; i++)
             {
-                third = first + second;
-                Console.Write(third+" ");
-                first = second;
-                second = third;
+                if (number % i == 0)
+                {
+                    a = a + i;
+                }
+                
             }
-           
+            if (a == number)
+            {
+                Console.WriteLine("Print it is a perfecT number");
+            }
+            else
+            {
+                Console.WriteLine("Print it is not a perfecT number");
+            }
+
+
         }
     }
 }
