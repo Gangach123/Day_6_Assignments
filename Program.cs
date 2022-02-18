8f9816f3f3bbf76b68dcc86776cf7295dacb19de
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-18 21:39:28 +0530

    Fibanocci_Series

--- Program.cs
+++ Program.cs
@@ -3,9 +3,22 @@ namespace probles
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to day 6 assignments");
+            int first=0;
+            int second=1;
+            int third=0;
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write(first + " " + second + " ");
+            for (int i = 2; i < number; i++)
+            {
+                third = first + second;
+                Console.Write(third+" ");
+                first = second;
+                second = third;
+            }
+           
         }
     }
 }
