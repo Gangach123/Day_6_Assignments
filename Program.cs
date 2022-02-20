4025e16e73d108ab74551bef042031afd2ad2464
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-20 22:49:22 +0530

    Monthly_Payment

--- Program.cs
+++ Program.cs
@@ -8,43 +8,29 @@ namespace Day6_Assignments
 {
      class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Enter 1 to convert temp. fahrenheit to celsius ");
-            Console.WriteLine("Enter 2 to convert temp. celsius to fahrenheit ");
-            int option = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter no of year");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nEnter principal");
+            double P = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nEnter interest rate");
+            double R = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("\nEnter a value");
-            double value = Convert.ToDouble(Console.ReadLine());
-
-            Conversion.TemperatureConversion(option, value);
-
-
+            payment.MonthlyPayment(Y, P, R);
 
         }
     }
-    class Conversion
+
+    class payment
     {
-
-        public static void TemperatureConversion(int option, double value)
+        public static void MonthlyPayment(double Y, double P, double R)
         {
-            double temp = 0;
-            if (option == 1)
-            {
-                temp = (value - 32) * 5 / 9;
+            double n = 12 * Y;
+            double r = R / (12 * 100);
 
-            }
-            else if (option == 2)
-            {
-                temp = (value * 9 / 5) + 32;
-            }
-            else
-            {
-                Console.WriteLine("Invalid Input");
-            }
-
-            Console.WriteLine("\n  Ans: " + temp);
-
+            double payment = P * r / (1 - Math.Pow((1 + r), (-n)));
+            Console.WriteLine("\nPayment:" + payment);
         }
 
 
