3ce1807b57b4370349d8ccfc623201314e0437cc
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-20 23:12:45 +0530

    Day_Of_Month

--- Program.cs
+++ Program.cs
@@ -8,13 +8,49 @@ namespace Day6_Assignments
 {
      class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Enter a Number : ");
-            int Number = Convert.ToInt16(Console.ReadLine());
-            double SqrtNumber = Math.Sqrt(Number);
-            Console.WriteLine("Square root of {0} is: {1}", Number, SqrtNumber);
-            Console.ReadLine();
+
+           
+            Console.WriteLine("enter day");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter month");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter year");
+            int y = Convert.ToInt32(Console.ReadLine());
+            GetWeek(d, m, y);
+        }
+        public static void GetWeek(int d, int m, int y)
+        {
+            int y1 = y - (14 - m) / 12;
+            int x = y1 + y1 / 4 - y1 / 100 + y1 / 400;
+            int m1 = m + 12 * ((14 - m) / 12) - 2;
+            int d1 = (d + x + 31 * m1 / 12) % 7;
+
+            switch (d1)
+            {
+                case 0:
+                    Console.WriteLine("sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("monday");
+                    break;
+                case 2:
+                    Console.WriteLine("tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("friday");
+                    break;
+                case 6:
+                    Console.WriteLine("saturday");
+                    break;
+            }
         }
 
     }
